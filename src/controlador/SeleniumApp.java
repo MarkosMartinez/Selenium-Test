3bf233277995ba4bd9851f0745d6010162ea6044
MarkosMartinez <ikbdk@plaiaundi.net>
2023-05-25 10:24:07 +0200

    First Commit

---
+++ src/controlador/SeleniumApp.java
@@ -0,0 +1,40 @@
+package controlador;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumApp {
+	
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("http://www.saucedemo.com");
+		
+		driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		
+		String texto = driver.findElement(By.className("error-message-container")).getText();
+		System.out.println(texto);
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		driver.quit();
+		
+	}
+
+}
